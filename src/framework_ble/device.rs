60a315fc083ed6bdd573d4e600e9206970faceef
pierre <>
2023-07-26 15:34:59 +0200

    typo

--- src/framework_ble/device.rs
+++ src/framework_ble/device.rs
@@ -2,6 +2,6 @@ use std::collections::HashMap;
 #[derive(Clone, Debug)]
 pub struct Device {
     pub name: String,
-    pub adress: [u8; 6],
+    pub address: [u8; 6],
     pub states: HashMap<String, String>,
 }
